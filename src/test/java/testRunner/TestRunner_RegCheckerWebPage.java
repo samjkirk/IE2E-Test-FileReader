53e97ba32d1978454ae09dab97207dc9eaa6baea
sam kirk <samueljkirk10@gmail.com>
2018-08-22 12:15:23 +0100

    refactor

--- src/test/java/testRunner/TestRunner_RegCheckerWebPage.java
+++ src/test/java/testRunner/TestRunner_RegCheckerWebPage.java
@@ -1,11 +1,11 @@
-package testRunners;
+package testRunner;
 
 import org.junit.runner.RunWith;
 import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features="src/test/java/features", glue="")
+@CucumberOptions(features="src/test/java/featureFiles", glue="")
 public class TestRunner_RegCheckerWebPage {
 
 }
